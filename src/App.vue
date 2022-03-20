33120b5009b0d7de985c537ab44c5cef3946bcbb
TanakitSupakam <96354376+TanakitSupakam@users.noreply.github.com>
2022-03-20 17:00:27 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      absolute
+      color="#6A76AB"
+      white
+      shrink-on-scroll
+      prominent
+      src="https://picsum.photos/1920/1080?random"
+      fade-img-on-scroll
+      scroll-target="#scrolling-techniques-3"
+      app
+      v-model="drawer"
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">MENU</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.to" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      absolute
+      color="#6A76AB"
+      white
+      shrink-on-scroll
+      prominent
+      src="https://picsum.photos/1920/1080?random"
+      fade-img-on-scroll
+      scroll-target="#scrolling-techniques-3"
+      app
+    >
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>MENU</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: "Home", icon: "mdi-home", to: "/" },
+      { title: "login", icon: "mdi-login", to: "/Login" },
+      {
+        title: "Register",
+        icon: "mdi-registered-trademark",
+        to: "/RegisterUser",
+      },
+      { title: "admin", icon: "mdi-laptop", to: "/about" },
+    ],
+  }),
+};
+</script>
